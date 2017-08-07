7114ea0ae9614fad4bfde8a8268e4a385330a17a
Michael Grenier <michael.grenier@live.com>
2017-08-06 20:07:55 -0400

    Immutable

--- src/lib/GameModeLobby.ts
+++ src/lib/GameModeLobby.ts
@@ -1,3 +1,5 @@
+import * as Immutable from 'immutable';
+
 import { GameMode } from './GameMode';
 import { Client } from './Client';
 import { Server } from './Server';
@@ -5,14 +7,12 @@ import { Message } from './Message';
 
 export class GameModeLobby extends GameMode {
 
-	protected players: Map<string, LobbyPlayer>;
-	protected clientPlayerId: string;
+	protected state: Immutable.Map<string, any>
 
 	constructor(adapter: Client | Server) {
 		super(adapter);
 
-		this.players = new Map<string, LobbyPlayer>();
-		this.clientPlayerId = '';
+		this.state = Immutable.Map();
 
 		if (this.isServer) {
 			const server: Server = this.adapter as Server;
@@ -30,10 +30,7 @@ export class GameModeLobby extends GameMode {
 	}
 
 	getState (): any {
-		return {
-			players: Array.from(this.players.values()),
-			clientPlayerId: this.clientPlayerId
-		};
+		return this.state.toJS();
 	}
 
 	onServerAction(action: Message, client: LobbyClient): void {
@@ -67,19 +64,21 @@ export class GameModeLobby extends GameMode {
 	}
 
 	onAction(action: Message): void {
-		
 		if (action.type === 'STATE') {
-			this.clientPlayerId = action.clientPlayerId;
-			this.players = new Map<string, LobbyPlayer>();
-			action.players.forEach((player: LobbyPlayer) => this.players.set(player.id, player));
+			this.state = this.state.withMutations(state => {
+				state.set('clientPlayerId', action.clientPlayerId);
+				const players = Immutable.Map<string, any>().asMutable();
+				action.players.forEach((player: LobbyPlayer) => players.set(player.id, Immutable.Map(player)));
+				state.set('players', players);
+			});
 		}
 
 		else if (action.type === 'JOIN') {
-			this.players.set(action.player.id, action.player);
+			this.state = this.state.update('players', (players) => players.set(action.player.id, Immutable.Map(action.player)));
 		}
 
 		else if (action.type === 'LEFT') {
-			this.players.delete(action.playerId);
+			this.state = this.state.deleteIn(['players', action.playerId]);
 		}
 
 		else if (action.type === 'MSG') {
