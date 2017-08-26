43e431addb852e21e674913488cadc39ea31fe86
Michael Grenier <michael.grenier@live.com>
2017-08-26 17:09:56 -0400

    Scene

--- src/nodeserver.ts
+++ src/nodeserver.ts
@@ -2,185 +2,205 @@ import { EventEmitter } from 'konstellio-eventemitter';
 import { Disposable, CompositeDisposable, IDisposable } from 'konstellio-disposable';
 import * as Immutable from 'immutable';
 
-import { Message } from './net/Message';
+import "reflect-metadata";
 
 interface Action {
 	type: string;
 	[payload: string]: any;
 }
 
+export function Replicated(type: typeof String | typeof Number | typeof Boolean) {
+	return function (entity: Entity, key: string) {
+		(entity.constructor as typeof Entity).replications.push([key, type]);
+	}
+}
+
+
 let nextId = 0;
 export function generateId(): string {
 	return (++nextId).toString();
 }
 
-export class World {
+export class Scene extends EventEmitter {
 
-	viewers: Immutable.List<Viewer>;
-	entities: Immutable.List<Entity>;
+	private entities: Immutable.Map<string, Entity>;
+	private types: Immutable.Map<string, new(...args) => Entity>;
 
-	constructor() {
-		this.viewers = Immutable.List<Viewer>();
-		this.entities = Immutable.List<Entity>();
+	constructor()
+	constructor(entities: Immutable.Map<string, Entity>, types: Immutable.Map<string, new(...args) => Entity>)
+	constructor(entities?: Immutable.Map<string, Entity>, types?: Immutable.Map<string, new(...args) => Entity>) {
+		super();
+
+		this.entities = entities || Immutable.Map<string, Entity>();
+		this.types = types || Immutable.Map<string, typeof Entity>();
 	}
 
-	toJSON(): any {
-		return {
-			entities: Array.from(this.entities.toArray()).map(entity => entity.toJSON())
-		};
-	}
-
-	addEntity(entity: Entity): void {
-		if (this.entities.indexOf(entity) === -1) {
-			this.entities = this.entities.push(entity);
-			entity.setWorld(this);
-			// TODO this.onEntityAdded
+	registerEntity(...entities: (new(...args) => Entity)[]): void {
+		const types = this.types.withMutations(types => {
+			entities.forEach(entity => types.set((entity as typeof Entity).handle, entity));
+		});
+		if (types !== this.types) {
+			this.types = types;
+			this.emit('onNewEntityRegistered', entities);
 		}
+	}
+
+	spawnEntity<E extends Entity>(handle: string, ...args: any[]): E {
+		if (this.types.has(handle) === false) {
+			throw new ReferenceError(`Entity ${handle} is not registered.`);
+		}
+		const type = this.types.get(handle);
+		const entity = new type(...args) as E;
+
+		this.entities = this.entities.set(entity.id, entity);
+		this.emit('onEntitySpawned', entity);
+		this.emit('onSceneChanged', [[undefined, entity]]);
+
+		return entity;
 	}
 
 	removeEntity(entity: Entity): void {
-		const idx = this.entities.indexOf(entity);
-		if (idx > -1) {
-			entity.unsetWorld();
-			this.entities = this.entities.delete(idx);
-			// TODO this.onEntityRemoved
+		if (this.entities.has(entity.id) === true) {
+			this.entities = this.entities.delete(entity.id);
+			this.emit('onEntityRemoved', entity);
+			this.emit('onSceneChanged', [[entity, undefined]]);
 		}
 	}
 
-	addViewer(viewer: Viewer): void {
-		if (this.viewers.indexOf(viewer) === -1) {
-			this.viewers = this.viewers.push(viewer);
-			viewer.setWorld(this);
-			// TODO this.onViewerAdded
+	getEntityById<E extends Entity>(id: string): E {
+		if (this.entities.has(id) === false) {
+			throw new ReferenceError(`Entity ${id} is not present in this scene.`);
 		}
+		return this.entities.get(id) as E;
 	}
 
-	removeViewer(viewer: Viewer): void {
-		const idx = this.viewers.indexOf(viewer);
-		if (idx > -1) {
-			viewer.unsetWorld();
-			this.viewers = this.viewers.delete(idx);
-			// TODO this.onViewerRemoved
+	tick(): void {
+		const entitiesChanged: [Entity, Entity][] = [];
+		const newEntities = this.entities.withMutations(entities => {
+			entities.forEach((entity: Entity, id: string) => {
+				const newEntity = entity.onTick();
+				if (newEntity !== entity) {
+					entitiesChanged.push([entity, newEntity]);
+					entities.set(id, newEntity);
+				}
+			});
+		});
+
+		if (newEntities !== this.entities) {
+			this.entities = newEntities;
+			this.emit('onSceneChanged', entitiesChanged);
 		}
 	}
 
 	dispatchAction(action: Action): void {
-		this.entities = this.entities.withMutations(entities => {
-			entities.forEach((entity: Entity, idx: number) => {
-				const newEntity = entity.doAction(action);
+		const entitiesChanged: [Entity, Entity][] = [];
+		const newEntities = this.entities.withMutations(entities => {
+			entities.forEach((entity: Entity, id: string) => {
+				const newEntity = entity.onAction(action);
 				if (newEntity !== entity) {
-					entities.set(idx, newEntity);
+					entitiesChanged.push([entity, newEntity]);
+					entities.set(id, newEntity);
 				}
 			});
 		});
+
+		if (newEntities !== this.entities) {
+			this.entities = newEntities;
+			this.emit('onSceneChanged', entitiesChanged);
+		}
 	}
+
 }
 
-export abstract class Entity {
+export class Entity {
+	static handle: string = 'Entity';
+	static replications: [string, typeof String | typeof Number | typeof Boolean][] = [];
 
 	readonly id: string;
-	protected world?: World;
 
+	constructor()
+	constructor(id: string | undefined)
 	constructor(id?: string) {
 		this.id = id || generateId();
 	}
 
-	toJSON(): any {
-		return {
-			id: this.id
-		};
+	onTick(): Entity {
+		return this;
 	}
 
-	setWorld(world: World): void {
-		this.world = world;
-		// TODO entity.onSpawned
+	onAction(action: Action): Entity {
+		return this;
 	}
 
-	unsetWorld(): void {
-		// TODO entity.onDespawned
-		this.world = undefined;
-	}
+	getDiff(previous?: Entity): Map<string, any> {
+		const replications = (this.constructor as typeof Entity).replications;
+		const diff: Map<string, any> = new Map<string, any>();
 
-	abstract doAction(action: Action): Entity;
-}
+		replications.forEach(([key, type]) => {
+			if (previous === undefined || this[key] !== previous[key]) {
+				diff.set(key, this[key]);
+			}
+		});
 
-export abstract class Viewer {
-
-	readonly id: string;
-	protected world?: World;
-
-	pawns: Immutable.Set<Entity>;
-
-	constructor(id?: string) {
-		this.id = id || generateId();
-		this.pawns = Immutable.Set<Entity>();
-	}
-
-	addPawn(entity: Entity): void {
-		if (this.pawns.has(entity) === false) {
-			this.pawns = this.pawns.add(entity);
-			// TODO this.onPawnAdded
-		}
-	}
-
-	removePawn(entity: Entity): void {
-		if (this.pawns.has(entity) === false) {
-			this.pawns = this.pawns.delete(entity);
-			// TODO this.onPawnRemoved
-		}
-	}
-
-	setWorld(world: World): void {
-		this.world = world;
-		// TODO entity.onSpawned
-	}
-
-	unsetWorld(): void {
-		// TODO entity.onDespawned
-		this.world = undefined;
+		return diff;
 	}
 }
 
-class PlayerViewer extends Viewer {
+export class Observer {
 
 }
 
-class PlayerEntity extends Entity {
 
+
+
+
+
+export class PlayerEntity extends Entity {
+	static handle: string = 'PlayerEntity';
+
+	@Replicated(String)
 	readonly name: string;
 
-	constructor(name: string, id?: string) {
+	@Replicated(Boolean)
+	readonly ready: boolean;
+
+	constructor(name: string, ready: boolean, id?: string) {
 		super(id);
+
 		this.name = name;
+		this.ready = !!ready;
 	}
 
-	toJSON(): any {
-		return Object.assign(super.toJSON(), {
-			name: this.name
-		});
-	}
-
-	doAction(action: Action): Entity {
-		if (action.type === 'HELLO' && action.name === this.name) {
-			return new PlayerEntity(`Hello${this.name}`, this.id);
+	onAction(action: Action): PlayerEntity {
+		if (action.type === 'READY' && action.id === this.id) {
+			return new PlayerEntity(this.name, action.ready, this.id);
 		}
 		return this;
 	}
 }
 
-const w = new World();
-const v1 = new PlayerViewer();
-const v2 = new PlayerViewer();
-const e1 = new PlayerEntity('Bob');
-const e2 = new PlayerEntity('George');
 
-v1.addPawn(e1);
-v2.addPawn(e2);
-w.addEntity(e1);
-w.addEntity(e2);
-w.addViewer(v1);
-w.addViewer(v2);
+const s = new Scene();
+s.registerEntity(PlayerEntity);
 
-console.log(w.toJSON());
-w.dispatchAction({ type: 'HELLO', name: 'Bob' });
-console.log(w.toJSON());
+s.on('onSceneChanged', (changes: [Entity, Entity][]) => {
+	changes.forEach(([previous, next]) => {
+		if (previous === undefined) {
+			console.log('Added', (next.constructor as typeof Entity).handle, next.id, next.getDiff(previous));
+		}
+		else if (next === undefined) {
+			console.log('Removed', previous.id);
+		}
+		else {
+			console.log('Updated', next.id, next.getDiff(previous));
+		}
+	})
+})
+
+const e1 = s.spawnEntity('PlayerEntity', 'Bob');
+const e2 = s.spawnEntity('PlayerEntity', 'George');
+const e3 = s.spawnEntity('PlayerEntity', 'Sophy');
+
+s.dispatchAction({ type: 'READY', id: e1.id, ready: true });
+s.removeEntity(s.getEntityById(e1.id));
+s.dispatchAction({ type: 'READY', id: e3.id, ready: true });
