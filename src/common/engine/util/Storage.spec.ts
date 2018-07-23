8061b6adb62c255f01600848f772fcab6d64499b
Michael Grenier <michael.grenier@live.com>
2018-07-23 08:26:56 -0400

    .

--- src/common/engine/util/Storage.spec.ts
+++ src/common/engine/util/Storage.spec.ts
@@ -11,14 +11,18 @@ describe('Storage', () => {
 		expect(() => {
 			return new Storage({}, {
 				size: 10,
-				move(block, offset) { }
+				move(data, block, offset) { },
+				set(data, index, value) { },
+				get(data, index) { },
 			});
 		}).to.not.throw();
 
 		expect(() => {
 			return new Storage({}, {
 				size: 0,
-				move(block, offset) { }
+				move(data, block, offset) { },
+				set(data, index, value) { },
+				get(data, index) { },
 			});
 		}).to.throw();
 
@@ -27,7 +31,9 @@ describe('Storage', () => {
 				initialSize: 0,
 				growSize: 10,
 				resize(data, size) { return data; },
-				move(block, offset, data) { }
+				move(data, block, offset) { },
+				set(data, index, value) { },
+				get(data, index) { },
 			});
 		}).to.not.throw();
 
@@ -36,7 +42,9 @@ describe('Storage', () => {
 				initialSize: 0,
 				growSize: 0,
 				resize(data, size) { return data; },
-				move(block, offset, data) { }
+				move(data, block, offset) { },
+				set(data, index, value) { },
+				get(data, index) { },
 			});
 		}).to.throw();
 
@@ -45,7 +53,9 @@ describe('Storage', () => {
 				initialSize: 0,
 				growSize: 10,
 				// resize(data, size) { },
-				move(block, offset) { }
+				move(block, offset) { },
+				set(data, index, value) { },
+				get(data, index) { },
 			} as any);
 		}).to.throw();
 
@@ -53,14 +63,28 @@ describe('Storage', () => {
 			initialSize: 4,
 			growSize: 4,
 			resize: resizeFloat32Array,
-			move(block, offset, data) {
+			move(data, block, offset) {
 				data.copyWithin(offset, block.offset, block.size);
-			}
+			},
+			set(data, index, value) { },
+			get(data, index) { },
 		});
 
 		expect(Array.from(s1.data)).to.eql([0, 0, 0, 0]);
 
-		const s2 = new Storage(
+		interface DataStruct {
+			sizes: Float32Array
+			positions: Float32Array
+			colors: Float32Array
+		}
+
+		interface DataItem {
+			size: number
+			positions: [number, number, number],
+			colors: [number, number, number, number]
+		}
+
+		const s2 = new Storage<DataStruct, DataItem>(
 			{
 				sizes: new Float32Array(0),
 				positions: new Float32Array(0),
@@ -74,11 +98,28 @@ describe('Storage', () => {
 				data.colors = resizeFloat32Array(data.colors, size * 4);
 				return data;
 			},
-			move(block, offset, data) {
+			move(data, block, offset) {
 				data.sizes.copyWithin(offset * 1, block.offset * 1, block.size * 1);
 				data.positions.copyWithin(offset * 3, block.offset * 3, block.size * 3);
 				data.colors.copyWithin(offset * 4, block.offset * 4, block.size * 4);
-			}
+			},
+			set(data, index, value) {
+				data.sizes[index * 1 + 0] = value.size;
+				data.positions[index * 3 + 0] = value.positions[0];
+				data.positions[index * 3 + 1] = value.positions[1];
+				data.positions[index * 3 + 2] = value.positions[2];
+				data.colors[index * 4 + 0] = value.colors[0];
+				data.colors[index * 4 + 1] = value.colors[1];
+				data.colors[index * 4 + 2] = value.colors[2];
+				data.colors[index * 4 + 3] = value.colors[3];
+			},
+			get(data, index) {
+				return {
+					size: data.sizes[index * 1 + 0],
+					positions: [data.positions[index * 3 + 0], data.positions[index * 3 + 1], data.positions[index * 3 + 2]],
+					colors: [data.colors[index * 4 + 0], data.colors[index * 4 + 1], data.colors[index * 4 + 2], data.colors[index * 4 + 3]]
+				};
+			},
 		});
 
 		expect(Array.from(s2.data.sizes)).to.eql([0, 0]);
--- src/common/engine/util/Storage.ts
+++ src/common/engine/util/Storage.ts
@@ -1,39 +1,43 @@
 import { Mutable } from "./Mutable";
 import { isArray } from "util";
 
-export type StorageOptions<T> = StorageOptionsResizable<T> | StorageOptionsFixed<T>
+export type StorageOptions<T, I> = StorageOptionsResizable<T, I> | StorageOptionsFixed<T, I>
 
-export interface StorageOptionsResizable<T> {
+export interface StorageOptionsResizable<T, I> {
 	initialSize?: number
 	growSize: number
 	resize: (data: T, size: number) => T
-	move: (block: Block<T>, offset: number, data: T) => void
+	move: (data: T, block: Block<T, I>, offset: number) => void
+	set: (data: T, index: number, item: I) => void
+	get: (data: T, index: number) => I
 }
 
-export interface StorageOptionsFixed<T> {
+export interface StorageOptionsFixed<T, I> {
 	size: number
-	move: (block: Block<T>, offset: number, data: T) => void
+	move: (data: T, block: Block<T, I>, offset: number) => void
+	set: (data: T, index: number, item: I) => void
+	get: (data: T, index: number) => I
 }
 
-function isResizableOptions(options: any): options is StorageOptionsResizable<any> {
+function isResizableOptions(options: any): options is StorageOptionsResizable<any, any> {
 	return typeof options.initialSize === 'number' && typeof options.resize === 'function';
 }
 
-function isFixedOptions(options: any): options is StorageOptionsFixed<any> {
+function isFixedOptions(options: any): options is StorageOptionsFixed<any, any> {
 	return typeof options.size !== 'undefined';
 }
 
-export class Storage<T = any> {
+export class Storage<T = any, I = any> {
 	protected static blockConstructor: typeof Block;
 
-	public readonly head: Block<T>;
-	public readonly tail: Block<T>;
+	public readonly head: Block<T, I>;
+	public readonly tail: Block<T, I>;
 
 	private autoGrow: boolean;
 
 	constructor(
 		public readonly data: T,
-		public readonly options: StorageOptions<T>
+		public readonly options: StorageOptions<T, I>
 	) {
 		let initialSize = 0;
 
@@ -57,41 +61,41 @@ export class Storage<T = any> {
 			throw new SyntaxError(`Storage options is neither StorageOptionsResizable, nor StorageOptionsFixed.`);
 		}
 
-		this.head = new Block<T>(this, true, 0, initialSize, undefined, undefined);
+		this.head = new Block<T, I>(this, true, 0, initialSize, undefined, undefined);
 		this.tail = this.head;
 	}
 
-	alloc(size: number): Block<T>
-	alloc(items: any[]): Block<T>
-	alloc(arg: number | any[]): Block<T> {
+	alloc(size: number): Block<T, I>
+	alloc(items: any[]): Block<T, I>
+	alloc(arg: number | any[]): Block<T, I> {
 		const size = typeof arg === 'number' ? arg : arg.length;
 		const items = isArray(arg) ? arg as number[] : undefined;
 
-		let allocBlock: Block<T> | undefined;
+		let allocBlock: Block<T, I> | undefined;
 
 		for (
-			let block: Block<T> | undefined = this.head;
+			let block: Block<T, I> | undefined = this.head;
 			block !== undefined;
 			block = block.right
 		) {
 			if (block.freed) {
 				if (block.size === size) {
 					allocBlock = block;
-					(allocBlock as Mutable<Block<T>>).freed = false;
+					(allocBlock as Mutable<Block<T, I>>).freed = false;
 					break;
 				}
 				else if (block.size > size) {
-					allocBlock = new Block<T>(this, false, block.offset, size, block.left, block);
+					allocBlock = new Block<T, I>(this, false, block.offset, size, block.left, block);
 					if (block.left) {
 						moveTo(block.left, block.left.offset, block.left.left, allocBlock);
 					}
 					moveTo(block, block.offset + size, allocBlock, block.right);
 
 					if (this.head === block) {
-						(this as Mutable<Storage<T>>).head = allocBlock;
+						(this as Mutable<Storage<T, I>>).head = allocBlock;
 					}
 					if (this.tail === block) {
-						(this as Mutable<Storage<T>>).tail = allocBlock;
+						(this as Mutable<Storage<T, I>>).tail = allocBlock;
 					}
 					break;
 				}
@@ -105,18 +109,18 @@ export class Storage<T = any> {
 			this.defrag();
 
 			const currentSize = this.tail.offset + this.tail.size;
-			const growSize = (this.options as StorageOptionsResizable<T>).growSize;
+			const growSize = (this.options as StorageOptionsResizable<T, I>).growSize;
 			const newSize = currentSize + Math.ceil(size / growSize) * growSize;
 
 			if (this.tail.freed) {
 				resizeTo(this.tail, this.tail.size + (newSize - currentSize));
 			} else {
-				const tail = new Block<T>(this, true, currentSize, newSize - currentSize, this.tail, undefined);
+				const tail = new Block<T, I>(this, true, currentSize, newSize - currentSize, this.tail, undefined);
 				moveTo(this.tail, this.tail.offset, this.tail.left, tail);
-				(this as Mutable<Storage<T>>).tail = tail;
+				(this as Mutable<Storage<T, I>>).tail = tail;
 			}
 
-			(this as Mutable<Storage<T>>).data = (this.options as StorageOptionsResizable<T>).resize(this.data, newSize);
+			(this as Mutable<Storage<T, I>>).data = (this.options as StorageOptionsResizable<T, I>).resize(this.data, newSize);
 
 			if (items) {
 				return this.alloc(items);
@@ -127,9 +131,9 @@ export class Storage<T = any> {
 		}
 	}
 
-	free(block: Block<T>) {
+	free(block: Block<T, I>) {
 		if (block.freed === false) {
-			(block as Mutable<Block<T>>).freed = true;
+			(block as Mutable<Block<T, I>>).freed = true;
 
 			let destroyBlock = false;
 
@@ -161,8 +165,8 @@ export class Storage<T = any> {
 	}
 
 	defrag() {
-		let block: Block<T> | undefined = this.head;
-		let left: Block<T> | undefined;
+		let block: Block<T, I> | undefined = this.head;
+		let left: Block<T, I> | undefined;
 		let offset = 0;
 		const currentSize = this.tail.offset + this.tail.size;
 
@@ -186,9 +190,9 @@ export class Storage<T = any> {
 		}
 
 		if (left && left.offset + left.size < currentSize) {
-			const tail = new Block<T>(this, true, left.offset + left.size, currentSize - (left.offset + left.size), left, undefined);
+			const tail = new Block<T, I>(this, true, left.offset + left.size, currentSize - (left.offset + left.size), left, undefined);
 			moveTo(left, left.offset, left.left, tail);
-			(this as Mutable<Storage<T>>).tail = tail;
+			(this as Mutable<Storage<T, I>>).tail = tail;
 		}
 	}
 
@@ -196,34 +200,47 @@ export class Storage<T = any> {
 		if (this.autoGrow && this.tail.freed) {
 			if (this.tail.left) {
 				const currentSize = this.tail.offset + this.tail.size;
-				const growSize = (this.options as StorageOptionsResizable<T>).growSize;
+				const growSize = (this.options as StorageOptionsResizable<T, I>).growSize;
 				const newSize = currentSize + Math.ceil(this.tail.offset / growSize) * growSize;
 
-				(this as Mutable<Storage<T>>).data = (this.options as StorageOptionsResizable<T>).resize(this.data, newSize);
+				(this as Mutable<Storage<T, I>>).data = (this.options as StorageOptionsResizable<T, I>).resize(this.data, newSize);
 			} else {
-				(this as Mutable<Storage<T>>).data = (this.options as StorageOptionsResizable<T>).resize(this.data, 0);
+				(this as Mutable<Storage<T, I>>).data = (this.options as StorageOptionsResizable<T, I>).resize(this.data, 0);
 			}
 			resizeTo(this.tail, 0);
 		}
 	}
 }
 
-export class Block<T = any> {
+export class Block<T = any, I = any> {
 	constructor(
-		public readonly storage: Storage<T>,
+		public readonly storage: Storage<T, I>,
 		public readonly freed: boolean,
 		public readonly offset: number,
 		public readonly size: number,
-		public readonly left: Block<T> | undefined,
-		public readonly right: Block<T> | undefined,
+		public readonly left: Block<T, I> | undefined,
+		public readonly right: Block<T, I> | undefined,
 	) {
 
+	}
+
+	set(index: number, ...values: I[]) {
+		const data = this.storage.data;
+		const setter = this.storage.options.set;
+		for (let i = 0, l = values.length; i < l; ++i) {
+			setter(data, this.offset + index + i, values[i]);
+		}
+	}
+
+	get(index: number): I {
+		// return this.memory.buffer[this.offset + index];
+		return this.storage.options.get(this.storage.data, index);
 	}
 }
 
 function moveTo(block: Block, offset: number, newLeft: Block | undefined, newRight: Block | undefined) {
 	if(offset !== block.offset) {
-		block.storage.options.move(block, offset, block.storage.data);
+		block.storage.options.move(block.storage.data, block, offset);
 		(block as Mutable<Block>).offset = offset;
 	}
 	(block as Mutable<Block>).left = newLeft;
