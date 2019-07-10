32aaceeb844631d78d4e845865855d528a11721c
Michael Grenier <michael@grenier.dev>
2019-07-09 21:19:10 -0400

    Engine first draft

--- packages/fexel/Engine.ts
+++ packages/fexel/Engine.ts
@@ -5,10 +5,9 @@ import { Texture } from './rendering/Texture';
 import { Scene } from './Scene';
 
 export class Engine implements IDisposable {
-	readonly mainScene?: Scene;
+	protected mainScene?: Scene;
 
 	constructor(public gl: WebGLRenderingContext, scene?: Scene) {
-		gl.clearColor(0, 0, 0, 0);
 		this.mainScene = scene;
 	}
 
@@ -18,13 +17,26 @@ export class Engine implements IDisposable {
 		return true;
 	}
 
+	loadScene(scene: Scene) {
+		// TODO Unload last scene ?
+		this.mainScene = scene;
+	}
+
 	render(): void {
 		const gl = this.gl;
 
-		gl.clear(gl.COLOR_BUFFER_BIT);
+		// From camera
+		const viewport = gl.getParameter(gl.VIEWPORT);
+		gl.viewport(0, 0, viewport[0], viewport[1]);
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+		// From material
+		gl.enable(gl.DEPTH_TEST);
+		gl.depthFunc(gl.LESS);
 		gl.enable(gl.BLEND);
 		gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+		gl.enable(gl.CULL_FACE);
 
 		if (this.mainScene) {
 			// Render scene...
