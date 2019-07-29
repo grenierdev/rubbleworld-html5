a47b76230d623edfd14b915596f89fd2742c7f1d
Michael Grenier <michael@grenier.dev>
2019-07-29 12:59:46 -0400

    Box2d

--- examples/physics2-stack/src/index.ts
+++ examples/physics2-stack/src/index.ts
@@ -10,10 +10,15 @@ import {
 	Physics2BoxColliderComponent,
 	Physics2EngineComponent,
 	Physics2CircleColliderComponent,
+	Physics2BodyType,
 } from '@fexel/core/components/Physics2';
 import { Box2 } from '@fexel/core/math/Box2';
 import { Vector2 } from '@fexel/core/math/Vector2';
 import { Circle } from '@fexel/core/math/Circle';
+import { Texture } from '@fexel/core/rendering/Texture';
+import { VertexShader, FragmentShader } from '@fexel/core/rendering/Shader';
+import { Material } from '@fexel/core/rendering/Material';
+import { MeshRendererComponent } from '@fexel/core/components/MeshRenderer';
 
 const stats = new Stats(340);
 stats.canvas.style.opacity = '0.9';
@@ -23,17 +28,64 @@ setInterval(() => stats.update(), 1000 / 30);
 const canvasEl = document.getElementById('canvas')! as HTMLCanvasElement;
 const engine = ((window as any).engine = new RenderableEngine(canvasEl, stats));
 
+const tex1 = new Texture({
+	data: document.getElementById('uvdebug')! as HTMLImageElement,
+});
+
+const vertShader = new VertexShader(
+	`
+		attribute vec3 vertPosition;
+		attribute vec2 vertUV1;
+
+		uniform mat4 projectionMatrix;
+		uniform mat4 viewMatrix;
+		uniform mat4 worldMatrix;
+
+		varying vec2 fragUV;
+
+		void main(void) {
+			fragUV = vertUV1;
+			gl_Position = projectionMatrix * viewMatrix * worldMatrix * vec4(vertPosition, 1.0);
+		}
+	`
+);
+const fragShader = new FragmentShader(
+	`
+		precision mediump float;
+
+		varying vec2 fragUV;
+		uniform sampler2D sampler;
+
+		void main(void) {
+			gl_FragColor = vec4(texture2D(sampler, fragUV).xyz, 0.25);
+		}
+	`
+);
+
+const mat = new Material(vertShader, fragShader);
+mat.twoSided = true;
+mat.transparent = false;
+mat.setUniform('sampler', tex1);
+
+const mesh = new Mesh({
+	vertices: new Float32Array([10.0, 10.0, 0.0, -10.0, 10.0, 0.0, 10.0, -10.0, 0.0, -10.0, -10.0, 0.0]),
+	indices: new Uint16Array([0, 1, 2, 2, 1, 3]),
+	uvs: [new Float32Array([1, 0, 0, 0, 1, 1, 0, 1])],
+	colors: [new Float32Array([1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1])],
+});
+
 const objs = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(
 	() =>
 		new Entity('Obj', [
 			new TransformComponent(
 				new Vector3(50 + (canvasEl.width - 100) * Math.random(), 50 + (canvasEl.height - 100) * Math.random(), 0)
 			),
-			new Physics2BodyComponent({ static: Math.random() > 0.5 }),
-			new Physics2BoxColliderComponent(
-				new Box2().setFromCenterAndSize(Vector2.Zero, new Vector2(10 + 15 * Math.random(), 10 + 15 * Math.random()))
-			),
+			new Physics2BodyComponent(Math.random() > 0.5 ? Physics2BodyType.Static : Physics2BodyType.Dynamic),
+			new Physics2CircleColliderComponent(10),
+			// new Physics2BoxColliderComponent(new Vector2(10, 10)),
+			// new Physics2BoxColliderComponent(new Vector2(10 + 15 * Math.random(), 10 + 15 * Math.random())),
 			// new Physics2CircleColliderComponent(new Circle(new Vector2(), 10 + 15 * Math.random())),
+			new MeshRendererComponent(mesh, mat),
 		])
 );
 
@@ -46,8 +98,8 @@ const walls = [
 	size =>
 		new Entity('Wall', [
 			new TransformComponent(new Vector3(size[0], size[1], 0)),
-			new Physics2BodyComponent({ static: true }),
-			new Physics2BoxColliderComponent(new Box2().setFromCenterAndSize(Vector2.Zero, new Vector2(size[2], size[3]))),
+			new Physics2BodyComponent(Physics2BodyType.Static),
+			new Physics2BoxColliderComponent(new Vector2(size[2], size[3])),
 		])
 );
 
--- packages/fexel/components/Physics2.ts
+++ packages/fexel/components/Physics2.ts
@@ -1,5 +1,4 @@
 import { Component, Scene, FixedUpdateContext, UpdateContext } from '../Scene';
-import { Engine, IEngineDefinition, ICollisionFilter, IEventCollision, Body, World, Bodies, Runner } from 'matter-js';
 import { Mutable } from '../util/Mutable';
 import { EventEmitter } from 'events';
 import { TransformComponent } from './Transform';
@@ -7,19 +6,22 @@ import { Vector2 } from '../math/Vector2';
 import { ReadonlyBox2 } from '../math/Box2';
 import { ReadonlyCircle } from '../math/Circle';
 import { lerp, smootherstep } from '../math/util';
+import { b2Body, b2BodyDef, b2BodyType } from '@fexel/box2d/Dynamics/b2Body';
+import { b2World } from '@fexel/box2d/Dynamics/b2World';
+import { b2FixtureDef } from '@fexel/box2d/Dynamics/b2Fixture';
+import { b2CircleShape } from '@fexel/box2d/Collision/Shapes/b2CircleShape';
+import { b2Shape } from '@fexel/box2d/Collision/Shapes/b2Shape';
+import { b2PolygonShape } from '@fexel/box2d/Collision/Shapes/b2PolygonShape';
 
 export class Physics2EngineComponent extends Component {
 	public executionOrder = 900;
 
-	public readonly engine: Engine;
-	public readonly runner: Runner;
+	public readonly world: b2World;
 
-	constructor(createOptions: IEngineDefinition = { enableSleeping: true }) {
+	constructor() {
 		super();
 
-		this.runner = Runner.create({});
-		this.engine = Engine.create(undefined, createOptions);
-		this.engine.world.gravity.y = -1;
+		this.world = new b2World(new Vector2(0, -10));
 	}
 
 	didMount() {
@@ -31,91 +33,71 @@ export class Physics2EngineComponent extends Component {
 
 	update(context: UpdateContext) {
 		if (context.debug) {
-			const bodies = this.engine.world.bodies;
-			for (const body of bodies) {
-				const position = body.position;
-
-				const wireframe: number[] = [];
-				for (let i = 1, l = body.vertices.length; i < l; ++i) {
-					wireframe.push(body.vertices[i - 1].x, body.vertices[i - 1].y, 0, body.vertices[i].x, body.vertices[i].y, 0);
-				}
-				wireframe.push(body.vertices[body.vertices.length - 1].x, body.vertices[body.vertices.length - 1].y, 0);
-				wireframe.push(body.vertices[0].x, body.vertices[0].y, 0);
-				context.debug.drawPrimitiveLines(wireframe, {
-					ttl: 0,
-					color: body.isSleeping ? [0.3, 0.3, 0.3, 0.5] : body.isStatic ? [0.6, 0.6, 0.6, 0.5] : [1.0, 1.0, 1.0, 0.5],
-				});
-
-				const axis: number[] = [
-					position.x,
-					position.y,
-					0,
-					(body.vertices[0].x + body.vertices[body.vertices.length - 1].x) / 2,
-					(body.vertices[0].y + body.vertices[body.vertices.length - 1].y) / 2,
-					0,
-				];
-				context.debug.drawPrimitiveLines(axis, {
-					ttl: 0,
-					color: [0.803921568627451, 0.3607843137254902, 0.3607843137254902, 0.5],
-				});
-			}
-
-			// for (const pair of this.engine.pairs.list) {
-			// 	if (!pair.isActive) {
-			// 		continue;
+			// for (let body = this.world.GetBodyList(); body; body = body.GetNext()) {
+			// 	const position = body.GetPosition();
+			// 	const asleep = !body.IsAwake;
+			// 	const type = body.GetType();
+			// 	const color: [number, number, number, number] = asleep
+			// 		? [0.3, 0.3, 0.3, 0.5]
+			// 		: type === b2BodyType.b2_staticBody
+			// 		? [0.6, 0.6, 0.6, 0.5]
+			// 		: [1.0, 1.0, 1.0, 0.5];
+			// 	for (let fixture = body.GetFixtureList(); fixture; fixture = fixture.GetNext()) {
+			// 		const shape = fixture.GetShape();
+			// 		if (shape instanceof b2CircleShape) {
+			// 		} else if (shape instanceof b2PolygonShape) {
+			// 			const vertices = shape.m_vertices;
+			// 			const wireframe: number[] = [];
+			// 			for (let i = 1, l = vertices.length; i < l; ++i) {
+			// 				wireframe.push(vertices[i - 1].x, vertices[i - 1].y, 0, vertices[i].x, vertices[i].y, 0);
+			// 			}
+			// 			wireframe.push(vertices[vertices.length - 1].x, vertices[vertices.length - 1].y, 0);
+			// 			wireframe.push(vertices[0].x, vertices[0].y, 0);
+			// 			context.debug.drawPrimitiveLines(wireframe, {
+			// 				ttl: 0,
+			// 				color,
+			// 			});
+			// 			const axis: number[] = [
+			// 				position.x,
+			// 				position.y,
+			// 				0,
+			// 				(vertices[0].x + vertices[vertices.length - 1].x) / 2,
+			// 				(vertices[0].y + vertices[vertices.length - 1].y) / 2,
+			// 				0,
+			// 			];
+			// 			context.debug.drawPrimitiveLines(axis, {
+			// 				ttl: 0,
+			// 				color: [0.803921568627451, 0.3607843137254902, 0.3607843137254902, 0.5],
+			// 			});
+			// 		}
 			// 	}
-			// 	console.log(pair);
 			// }
 		}
 	}
 
 	fixedUpdate(context: FixedUpdateContext) {
-		this.engine.timing.timeScale = context.timeScale;
-		Runner.tick(this.runner, this.engine, context.fixedTime);
-		console.error('https://github.com/flyover/box2d.ts/tree/master/Box2D');
+		this.world.Step(context.fixedDeltaTime, 8, 3);
 	}
+}
+
+export enum Physics2BodyType {
+	Dynamic,
+	Static,
+	Kinematic,
 }
 
 export class Physics2BodyComponent extends Component {
 	public executionOrder = 910;
 
-	public readonly body: Body;
+	public readonly body: b2Body | undefined;
 
 	public readonly transform: TransformComponent | undefined;
 	public readonly engine: Physics2EngineComponent | undefined;
-	public readonly collisionFilter: ICollisionFilter = {
-		category: 1,
-		group: 0,
-		mask: -1,
-	};
-	public density = 0.001;
-	public friction = 0.1;
-	public frictionAir = 0.01;
-	public frictionStatic = 0.5;
-	public restitution = 0;
-	public timeScale = 1;
-	public sensor = false;
-	public static = false;
 
 	protected emitter = new EventEmitter();
 
-	constructor(
-		options?: Partial<
-			Pick<
-				Physics2BodyComponent,
-				'density' | 'friction' | 'frictionAir' | 'frictionStatic' | 'restitution' | 'timeScale' | 'sensor' | 'static'
-			>
-		>
-	) {
+	constructor(public type = Physics2BodyType.Static) {
 		super();
-
-		if (options) {
-			for (const key in options) {
-				this[key] = options[key];
-			}
-		}
-
-		this.body = Body.create({});
 	}
 
 	didMount() {
@@ -124,40 +106,54 @@ export class Physics2BodyComponent extends Component {
 			this.transform.breakParentChain = true;
 		}
 		(this as Mutable<Physics2BodyComponent>).engine = this.entity!.scene!.getComponent(Physics2EngineComponent);
-		if (this.engine && this.body) {
-			World.add(this.engine.engine.world, this.body);
+		if (this.engine) {
+			this.updateBody();
 		}
-
-		this.updateBody();
 	}
 
 	willUnmount() {
 		if (this.engine && this.body) {
-			World.remove(this.engine.engine.world, this.body);
+			this.engine.world.DestroyBody(this.body);
 		}
 	}
 
 	updateBody() {
-		Body.setStatic(this.body, this.static);
-		Body.setDensity(this.body, this.density);
-		this.body.friction = this.friction;
-		this.body.frictionAir = this.frictionAir;
-		this.body.frictionStatic = this.frictionStatic;
-		this.body.restitution = this.restitution;
-		this.body.timeScale = this.timeScale;
-		this.body.isSensor = this.sensor;
+		if (this.engine) {
+			const bodyDef = new b2BodyDef();
+			switch (this.type) {
+				case Physics2BodyType.Dynamic:
+					bodyDef.type = b2BodyType.b2_dynamicBody;
+					break;
+				case Physics2BodyType.Static:
+					bodyDef.type = b2BodyType.b2_staticBody;
+					break;
+				case Physics2BodyType.Kinematic:
+					bodyDef.type = b2BodyType.b2_kinematicBody;
+					break;
+			}
 
-		const colliders = this.getComponents(Physics2ColliderComponent, true);
-		const parts = colliders.map(collider => collider.body);
-		Body.setParts(this.body, parts);
+			if (this.transform) {
+				bodyDef.position.Set(this.transform.localPosition.x, this.transform.localPosition.y);
+				bodyDef.angle = this.transform.localRotation.z;
+			}
 
-		if (this.transform) {
-			Body.setPosition(this.body, this.transform.localPosition);
-			Body.setAngle(this.body, this.transform.localRotation.z);
+			if (!this.body) {
+				(this as Mutable<Physics2BodyComponent>).body = this.engine!.world.CreateBody(bodyDef);
+			} else {
+				for (let fixture = this.body.GetFixtureList(); fixture; fixture = fixture.GetNext()) {
+					this.body.DestroyFixture(fixture);
+				}
+				this.body.SetType(bodyDef.type);
+				this.body.SetPosition(bodyDef.position);
+				this.body.SetAngle(bodyDef.angle);
+			}
+
+			const colliders = this.getComponents(Physics2ColliderComponent, true);
+			const fixtureDefs = colliders.map(collider => collider.updateFixtureDef());
+			for (const fixtureDef of fixtureDefs) {
+				this.body!.CreateFixture(fixtureDef);
+			}
 		}
-
-		Body.setAngularVelocity(this.body, 0);
-		Body.setVelocity(this.body, Vector2.Zero);
 	}
 
 	update(context: FixedUpdateContext) {
@@ -167,12 +163,15 @@ export class Physics2BodyComponent extends Component {
 				0,
 				1
 			);
+			const position = this.body.GetPosition();
+			const angle = this.body.GetAngle();
+
 			this.transform.localPosition.set(
-				lerp(this.transform.localPosition.x, this.body.position.x, alpha),
-				lerp(this.transform.localPosition.y, this.body.position.y, alpha),
+				lerp(this.transform.localPosition.x, position.x, alpha),
+				lerp(this.transform.localPosition.y, position.y, alpha),
 				0
 			);
-			this.transform.localRotation.set(0, 0, lerp(this.transform.localRotation.z, this.body.angle, alpha));
+			this.transform.localRotation.set(0, 0, lerp(this.transform.localRotation.z, angle, alpha));
 		}
 	}
 
@@ -184,53 +183,78 @@ export class Physics2BodyComponent extends Component {
 		return this.emitter.on('onSleepEnd', handler);
 	}
 
-	onCollisionStart(handler: (collision: IEventCollision<Engine>) => void) {
+	onCollisionStart(handler: (collision: unknown) => void) {
 		return this.emitter.on('onCollisionStart', handler);
 	}
 
-	onCollisionEnd(handler: (collision: IEventCollision<Engine>) => void) {
+	onCollisionEnd(handler: (collision: unknown) => void) {
 		return this.emitter.on('onCollisionStart', handler);
 	}
+}
+
+export interface Physics2Filter {
+	categoryBits: number;
+	maskBits: number;
+	groupIndex: number;
 }
 
 export abstract class Physics2ColliderComponent extends Component {
-	abstract readonly body: Body;
+	public readonly fixtureDef: b2FixtureDef;
+
+	constructor(
+		public shape: b2Shape,
+		public density = 0,
+		public friction = 0.2,
+		public restitution = 0,
+		public isSensor = false,
+		public filter: Physics2Filter = {
+			categoryBits: 0x0001,
+			maskBits: 0xffff,
+			groupIndex: 0,
+		}
+	) {
+		super();
+
+		this.fixtureDef = new b2FixtureDef();
+		this.updateFixtureDef();
+	}
+
+	updateFixtureDef() {
+		this.fixtureDef.shape = this.shape;
+		this.fixtureDef.density = this.density;
+		this.fixtureDef.friction = this.friction;
+		this.fixtureDef.restitution = this.restitution;
+		this.fixtureDef.isSensor = this.isSensor;
+		this.fixtureDef.filter.Copy(this.filter);
+		return this.fixtureDef;
+	}
 }
 
 export class Physics2BoxColliderComponent extends Physics2ColliderComponent {
-	public readonly body: Body;
-
-	constructor(public readonly box: ReadonlyBox2) {
-		super();
-
-		const center = new Vector2();
-		const size = new Vector2();
-		box.getCenter(center);
-		box.getSize(size);
-		this.body = Bodies.rectangle(center.x, center.y, size.x, size.y);
+	constructor(
+		public readonly size: Vector2,
+		public readonly center = Vector2.Zero.clone(),
+		public readonly angle = 0,
+		density?: number,
+		friction?: number,
+		restitution?: number,
+		isSensor?: boolean,
+		filter?: Physics2Filter
+	) {
+		super(new b2PolygonShape(), density, friction, restitution, isSensor, filter);
+		(this.shape as b2PolygonShape).SetAsBox(size.x, size.y, center, angle);
 	}
 }
 
 export class Physics2CircleColliderComponent extends Physics2ColliderComponent {
-	public readonly body: Body;
-
-	constructor(public circle: ReadonlyCircle) {
-		super();
-
-		this.body = Bodies.circle(circle.center.x, circle.center.y, circle.radius);
-	}
-}
-
-export class Physics2CapsuleColliderComponent extends Physics2ColliderComponent {
-	public readonly body: Body;
-
-	constructor(public readonly capsule: ReadonlyBox2, public readonly radius = 0) {
-		super();
-
-		const center = new Vector2();
-		const size = new Vector2();
-		capsule.getCenter(center);
-		capsule.getSize(size);
-		this.body = Bodies.rectangle(center.x, center.y, size.x, size.y, { chamfer: { radius } });
+	constructor(
+		public readonly radius: number,
+		density?: number,
+		friction?: number,
+		restitution?: number,
+		isSensor?: boolean,
+		filter?: Physics2Filter
+	) {
+		super(new b2CircleShape(radius), density, friction, restitution, isSensor, filter);
 	}
 }
